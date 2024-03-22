f8bd8d475ad4b626a3c1b6ec36147ee5f6d14f74
Froncu <jakubfratczak204@gmail.com>
2024-03-22 21:16:28 +0100

    [W.I.P. FEATURE] add DirtyData class which encapsulates the dirty flag pattern

---
+++ Fronge/DirtyData.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <functional>
+
+namespace fro
+{
+	template<typename PrimaryDataType, typename DerivedDataType = PrimaryDataType>
+	class DirtyData final
+	{
+	public:
+		DirtyData(std::function<void(PrimaryDataType&, DerivedDataType&)> derivedDataCalculator) :
+			m_DerivedDataCalculator{ derivedDataCalculator }
+		{
+		};
+
+		DirtyData(const DirtyData&) = default;
+		DirtyData(DirtyData&&) noexcept = default;
+
+		~DirtyData() = default;
+
+		DirtyData& operator=(const DirtyData&) = default;
+		DirtyData& operator=(DirtyData&&) noexcept = default;
+
+		void calculateDerivedData()
+		{
+			m_DerivedDataCalculator(m_PrimaryData, m_DerivedData);
+		}
+
+		void setPrimaryData(PrimaryDataType&& value)
+		{
+			m_PrimaryData = std::forward(value);
+			m_IsDerivedDataDirty = true;
+		}
+
+		const DerivedDataType& getPrimaryData() const
+		{
+			return m_PrimaryData;
+		}
+
+		const PrimaryDataType& getDerivedData()
+		{
+			if (m_IsDerivedDataDirty)
+				calculateDerivedData();
+			
+			return m_DerivedData;
+		}
+
+		bool isDirty() const
+		{
+			return m_IsDerivedDataDirty;
+		}
+
+	private:
+		PrimaryDataType m_PrimaryData;
+		DerivedDataType m_DerivedData;
+		std::function<void(const PrimaryDataType&, DerivedDataType&)> m_DerivedDataCalculator;
+
+		bool m_IsDerivedDataDirty{ true };
+	};
+}
