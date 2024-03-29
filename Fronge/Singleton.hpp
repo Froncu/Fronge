4d92be8233ee238f453871bd4fae7bf502e31405
Froncu <jakubfratczak204@gmail.com>
2024-03-29 18:57:49 +0100

    [FEATURE] add singleton defines

--- Fronge/Singleton.hpp
+++ Fronge/Singleton.hpp
@@ -25,3 +25,29 @@ namespace fro
 		Singleton& operator=(Singleton&&) noexcept = delete;
 	};
 }
+
+#ifndef fro_GENERATED_SINGLETON_BODY(identifier)
+#define fro_GENERATED_SINGLETON_BODY(identifier)\
+friend class Singleton<identifier>;\
+\
+public:\
+	virtual ~identifier() override;\
+\
+private:\
+	identifier();\
+	identifier(const identifier&) = delete;\
+	identifier(identifier&&) noexcept = delete;\
+\
+	identifier& operator=(const identifier&) = delete;\
+	identifier& operator=(identifier&&) noexcept = delete;
+#endif
+
+#ifndef fro_GENERATED_SINGLETON_CONSTRUCTOR
+#define fro_GENERATED_SINGLETON_CONSTRUCTOR(identifier)\
+fro::identifier::identifier()
+#endif
+
+#ifndef fro_GENERATED_SINGLETON_DESTRUCTOR
+#define fro_GENERATED_SINGLETON_DESTRUCTOR(identifier)\
+fro::identifier::~identifier()
+#endif
