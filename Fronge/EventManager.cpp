a4f6ba7d9264862d8cb820f6264f5725f8c10207
Froncu <jakubfratczak204@gmail.com>
2024-04-08 11:27:47 +0200

    [FIX] initialise & quit the necessary subsystem inside the event manager

--- Fronge/EventManager.cpp
+++ Fronge/EventManager.cpp
@@ -3,15 +3,21 @@
 #include "GUIContext.h"
 #include "InputManager.h"
 
+#include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
+#include <stdexcept>
+#include <format>
 
 #pragma region Constructors/Destructor
 fro_GENERATED_SINGLETON_CONSTRUCTOR(EventManager)
 {
+	if (SDL_InitSubSystem(SDL_INIT_EVENTS) != 0)
+		throw std::runtime_error(std::format("[ SDL_InitSubSystem() FAILED ] -> {}", SDL_GetError()));
 }
 
 fro_GENERATED_SINGLETON_DESTRUCTOR(EventManager)
 {
+	SDL_QuitSubSystem(SDL_INIT_EVENTS);
 }
 #pragma endregion Constructors/Destructor
 
