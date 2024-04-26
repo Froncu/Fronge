bfa1f6e820867b628fdcd1f213a17633939c8e65
Froncu <jakubfratczak204@gmail.com>
2024-04-26 15:40:33 +0200

    [FEATURE] add a templated event queue class

---
+++ Fronge/Code/EventQueue.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <functional>
+#include <deque>
+#include <algorithm>
+
+namespace fro
+{
+	template<typename EventType, std::invocable<EventType&> EventProcesserType, bool unique = std::equality_comparable<EventType>>
+	class EventQueue final
+	{
+	public:
+		EventQueue(EventProcesserType eventProcesser)
+			: m_EventProcesser{ eventProcesser }
+		{
+		}
+
+		EventQueue(const EventQueue&) = default;
+		EventQueue(EventQueue&&) noexcept = default;
+
+		~EventQueue() = default;
+
+		EventQueue& operator=(const EventQueue&) = default;
+		EventQueue& operator=(EventQueue&&) noexcept = default;
+
+		void pushEvent(EventType&& event)
+		{
+			if constexpr (unique)
+				if (!isEventUnqiue(event))
+					return;
+
+			m_dEvents.emplace_back(std::move(event));
+		}
+
+		void pushEvent(const EventType& event)
+		{
+			if constexpr (unique)
+				if (!isEventUnqiue(event))
+					return;
+
+			m_dEvents.push_back(event);
+		}
+
+		void processEvents()
+		{
+			while (!m_dEvents.empty())
+			{
+				m_EventProcesser(m_dEvents.front());
+				m_dEvents.pop_front();
+			}
+		}
+
+		const std::deque<EventType>& getQueue() const
+		{
+			return m_dEvents;
+		}
+
+	private:
+		bool isEventUnqiue(const EventType& event)
+		{
+			return std::none_of(m_dEvents.begin(), m_dEvents.end(),
+				[&event](const EventType& queuedEvent)
+				{
+					return event == queuedEvent;
+				});
+		}
+
+		EventProcesserType m_EventProcesser{};
+		std::deque<EventType> m_dEvents{};
+	};
+}
