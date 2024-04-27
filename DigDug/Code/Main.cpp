fcbc226b78912505a65ac7e5571b53095553fc8b
Froncu <jakubfratczak204@gmail.com>
2024-04-27 14:51:49 +0200

    [FIX] use try catch

--- DigDug/Code/Main.cpp
+++ DigDug/Code/Main.cpp
@@ -1,5 +1,6 @@
 #include "AudioSDL.h"
 #include "AudioService.h"
+#include "Console.hpp"
 #include "FPSCounter.h"
 #include "Fronge.h"
 #include "GameObject.h"
@@ -22,40 +23,48 @@
 
 int main(int, char**)
 {
-	auto& audioService{ fro::ServiceLocator<fro::AudioService>::getInstance() };
+	try
+	{
+		auto& audioService{ fro::ServiceLocator<fro::AudioService>::getInstance() };
 
-	audioService.setProvider<fro::AudioSDL>();
-	audioService.getService().playMusic("BGM #01.mp3", 0.1f);
+		audioService.setProvider<fro::AudioSDL>();
+		audioService.getService().playMusic("BGM #01.mp3", 0.1f);
 
-	fro::Scene& scene{ fro::SceneManager::getInstance().addScene() };
+		fro::Scene& scene{ fro::SceneManager::getInstance().addScene() };
 
-	fro::GameObject& player1{ scene.addGameObject() };
-	player1.addComponent<fro::Sprite>()->setFileName("DigDug.png");
-	player1.getComponent<fro::Transform>()->setLocalPosition({ 300, 300 });
+		fro::GameObject& player1{ scene.addGameObject() };
+		player1.addComponent<fro::Sprite>()->setFileName("DigDug.png");
+		player1.getComponent<fro::Transform>()->setLocalPosition({ 300, 300 });
 
-	fro::InputManager::getInstance().bindKeyInputToAction({ SDL_SCANCODE_D, fro::ButtonInput::State::down }, "moveRight");
-	fro::InputManager::getInstance().bindKeyInputToAction({ SDL_SCANCODE_A, fro::ButtonInput::State::down }, "moveLeft");
-	fro::InputManager::getInstance().bindKeyInputToAction({ SDL_SCANCODE_W, fro::ButtonInput::State::down }, "moveUp");
-	fro::InputManager::getInstance().bindKeyInputToAction({ SDL_SCANCODE_S, fro::ButtonInput::State::down }, "moveDown");
+		fro::InputManager::getInstance().bindKeyInputToAction({ SDL_SCANCODE_D, fro::ButtonInput::State::down }, "moveRight");
+		fro::InputManager::getInstance().bindKeyInputToAction({ SDL_SCANCODE_A, fro::ButtonInput::State::down }, "moveLeft");
+		fro::InputManager::getInstance().bindKeyInputToAction({ SDL_SCANCODE_W, fro::ButtonInput::State::down }, "moveUp");
+		fro::InputManager::getInstance().bindKeyInputToAction({ SDL_SCANCODE_S, fro::ButtonInput::State::down }, "moveDown");
 
-	fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveRight", player1).setMoveDirection({ 1.0f, 0.0f });
-	fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveLeft", player1).setMoveDirection({ -1.0f, 0.0f });
-	fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveUp", player1).setMoveDirection({ 0.0f, -1.0f });
-	fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveDown", player1).setMoveDirection({ 0.0f, 1.0f });
+		fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveRight", player1).setMoveDirection({ 1.0f, 0.0f });
+		fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveLeft", player1).setMoveDirection({ -1.0f, 0.0f });
+		fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveUp", player1).setMoveDirection({ 0.0f, -1.0f });
+		fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveDown", player1).setMoveDirection({ 0.0f, 1.0f });
 
-	fro::GameObject& player2{ scene.addGameObject() };
-	player2.addComponent<fro::Sprite>()->setFileName("DigDug.png");
-	player2.getComponent<fro::Transform>()->setLocalPosition({ 200, 300 });
+		fro::GameObject& player2{ scene.addGameObject() };
+		player2.addComponent<fro::Sprite>()->setFileName("DigDug.png");
+		player2.getComponent<fro::Transform>()->setLocalPosition({ 200, 300 });
 
-	fro::InputManager::getInstance().bindKeyInputToAction({ SDL_CONTROLLER_BUTTON_DPAD_RIGHT, fro::ButtonInput::State::down }, "moveRightController");
-	fro::InputManager::getInstance().bindKeyInputToAction({ SDL_CONTROLLER_BUTTON_DPAD_LEFT, fro::ButtonInput::State::down }, "moveLeftController");
-	fro::InputManager::getInstance().bindKeyInputToAction({ SDL_CONTROLLER_BUTTON_DPAD_UP, fro::ButtonInput::State::down }, "moveUpController");
-	fro::InputManager::getInstance().bindKeyInputToAction({ SDL_CONTROLLER_BUTTON_DPAD_DOWN, fro::ButtonInput::State::down }, "moveDownController");
+		fro::InputManager::getInstance().bindKeyInputToAction({ SDL_CONTROLLER_BUTTON_DPAD_RIGHT, fro::ButtonInput::State::down }, "moveRightController");
+		fro::InputManager::getInstance().bindKeyInputToAction({ SDL_CONTROLLER_BUTTON_DPAD_LEFT, fro::ButtonInput::State::down }, "moveLeftController");
+		fro::InputManager::getInstance().bindKeyInputToAction({ SDL_CONTROLLER_BUTTON_DPAD_UP, fro::ButtonInput::State::down }, "moveUpController");
+		fro::InputManager::getInstance().bindKeyInputToAction({ SDL_CONTROLLER_BUTTON_DPAD_DOWN, fro::ButtonInput::State::down }, "moveDownController");
 
-	fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveRightController", player2).setMoveDirection({ 1.0f, 0.0f });
-	fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveLeftController", player2).setMoveDirection({ -1.0f, 0.0f });
-	fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveUpController", player2).setMoveDirection({ 0.0f, -1.0f });
-	fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveDownController", player2).setMoveDirection({ 0.0f, 1.0f });
+		fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveRightController", player2).setMoveDirection({ 1.0f, 0.0f });
+		fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveLeftController", player2).setMoveDirection({ -1.0f, 0.0f });
+		fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveUpController", player2).setMoveDirection({ 0.0f, -1.0f });
+		fro::InputManager::getInstance().bindActionToCommand<fro::MoveCommand>("moveDownController", player2).setMoveDirection({ 0.0f, 1.0f });
 
-	return fro::Fronge::getInstance().run();
+		return fro::Fronge::getInstance().run();
+	}
+	catch (std::runtime_error const& exception)
+	{
+		fro::Console::getInstance().log(exception.what(), fro::Console::BackgroundColor::DarkRed);
+		return 1;
+	}
 }
--- Spiral/Code/Main.cpp
+++ Spiral/Code/Main.cpp
@@ -1,3 +1,4 @@
+#include "Console.hpp"
 #include "Fronge.h"
 #include "GameObject.h"
 #include "Rotator.h"
@@ -8,29 +9,39 @@
 
 #include <sdl_main.h>
 
+#include <stdexcept>
+
 int main(int, char**)
 {
-	fro::Scene& scene{ fro::SceneManager::getInstance().addScene() };
-
-	fro::GameObject* pGameObject;
-	pGameObject = &scene.addGameObject();
-	pGameObject->addComponent<fro::Sprite>()->setFileName("logo.tga");
-	pGameObject->getComponent<fro::Transform>()->setLocalPosition({ 320, 240 });
-
-	for (std::size_t index{}; index < 500; ++index)
+	try
 	{
-		float constexpr basePeriod{ 2.0f };
-		float constexpr childPeriodDelayMultiplier{ 0.002f };
+		fro::Scene& scene{ fro::SceneManager::getInstance().addScene() };
 
-		fro::GameObject& gameObject{ scene.addGameObject() };
-		gameObject.addComponent<fro::Sprite>()->setFileName("logo.tga");
-		fro::Rotator* const pRotator{ gameObject.addComponent<fro::Rotator>() };
-		pRotator->setPeriod(basePeriod + index * childPeriodDelayMultiplier);
-		pRotator->setRadius(2.0f);
+		fro::GameObject* pGameObject;
+		pGameObject = &scene.addGameObject();
+		pGameObject->addComponent<fro::Sprite>()->setFileName("logo.tga");
+		pGameObject->getComponent<fro::Transform>()->setLocalPosition({ 320, 240 });
 
-		gameObject.setParent(pGameObject, false);
-		pGameObject = &gameObject;
+		for (std::size_t index{}; index < 500; ++index)
+		{
+			float constexpr basePeriod{ 2.0f };
+			float constexpr childPeriodDelayMultiplier{ 0.002f };
+
+			fro::GameObject& gameObject{ scene.addGameObject() };
+			gameObject.addComponent<fro::Sprite>()->setFileName("logo.tga");
+			fro::Rotator* const pRotator{ gameObject.addComponent<fro::Rotator>() };
+			pRotator->setPeriod(basePeriod + index * childPeriodDelayMultiplier);
+			pRotator->setRadius(2.0f);
+
+			gameObject.setParent(pGameObject, false);
+			pGameObject = &gameObject;
+		}
+
+		return fro::Fronge::getInstance().run();
 	}
-
-	return fro::Fronge::getInstance().run();
+	catch (std::runtime_error const& exception)
+	{
+		fro::Console::getInstance().log(exception.what(), fro::Console::BackgroundColor::DarkRed);
+		return 1;
+	}
 }
