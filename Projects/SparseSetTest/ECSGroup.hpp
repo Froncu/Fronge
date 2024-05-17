1765dea553022e8f8baa55d787bb59eaf590f68b
Froncu <jakubfratczak204@gmail.com>
2024-05-18 01:59:55 +0200

    [FEATURE] enforce complete distinction between owned & observed types

--- Projects/SparseSetTest/ECSGroup.hpp
+++ Projects/SparseSetTest/ECSGroup.hpp
@@ -1,6 +1,7 @@
 #if not defined fro_ECS_GROUP_H
 #define fro_ECS_GROUP_H
 
+#include <tuple>
 #include <vector>
 
 namespace fro
@@ -23,8 +24,15 @@ namespace fro
 			(not std::is_same_v<Head, Tail> and ...) and isPackUnique<Tail...>
 		};
 
-		static_assert(isPackUnique<OwnedTypes...>::value);
-		static_assert(isPackUnique<ObservedTypes...>::value);
+		static_assert(isPackUnique<OwnedTypes...>);
+		static_assert(isPackUnique<ObservedTypes...>);
+
+		template<typename Type, typename... Pack>
+		static auto constexpr isContainedInPack{ (std::is_same_v<Type, Pack> or ...) };
+
+		static_assert(
+			(not isContainedInPack<OwnedTypes, ObservedTypes...> and ...) and
+			(not isContainedInPack<ObservedTypes, OwnedTypes...> and ...));
 
 	public:
 		consteval ECSGroup() = default;
