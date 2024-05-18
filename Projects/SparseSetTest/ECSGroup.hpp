77b4a635c144dac472724ec10b5cb0b01bace068
Froncu <jakubfratczak204@gmail.com>
2024-05-18 12:44:39 +0200

    [FEATURE] add descriptive messages to static asserts

--- Projects/SparseSetTest/ECSGroup.hpp
+++ Projects/SparseSetTest/ECSGroup.hpp
@@ -1,6 +1,7 @@
 #if not defined fro_ECS_GROUP_H
 #define fro_ECS_GROUP_H
 
+#include <iostream>
 #include <tuple>
 #include <vector>
 
@@ -24,15 +25,16 @@ namespace fro
 			(not std::is_same_v<Head, Tail> and ...) and isPackUnique<Tail...>
 		};
 
-		static_assert(isPackUnique<OwnedTypes...>);
-		static_assert(isPackUnique<ObservedTypes...>);
+		static_assert(isPackUnique<OwnedTypes...>, "the specified pack of owned component types is not unique");
+		static_assert(isPackUnique<ObservedTypes...>, "the specified pack of observed component types pack is not unique");
 
 		template<typename Type, typename... Pack>
 		static auto constexpr isContainedInPack{ (std::is_same_v<Type, Pack> or ...) };
 
 		static_assert(
 			(not isContainedInPack<OwnedTypes, ObservedTypes...> and ...) and
-			(not isContainedInPack<ObservedTypes, OwnedTypes...> and ...));
+			(not isContainedInPack<ObservedTypes, OwnedTypes...> and ...),
+			"there is no reason to observe any of the owned component types in the same group");
 
 	public:
 		consteval ECSGroup() = default;
