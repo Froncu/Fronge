1cf022dfe8039afec155b3b44b04d5dc607d955c
Froncu <jakubfratczak204@gmail.com>
2024-05-19 16:39:30 +0200

    [FEATURE] bind add and remove NPC functions to the Lua script

--- Projects/LuaScript/Source/LuaScript.cpp
+++ Projects/LuaScript/Source/LuaScript.cpp
@@ -1,9 +1,51 @@
+#include "Console.hpp"
+#include "NPCManager.h"
 #include "Typenames.hpp"
 
 #include <lua.hpp>
 
+#include <iostream>
+#include <format>
+
+static int addNPC(lua_State* const pState)
+{
+	auto const NPCName{ lua_tostring(pState, 1) };
+	NPCManager::getInstance().addNPC(NPCName);
+
+#if not defined NDEBUG
+	auto const message{ std::format("NPC named {} added!", NPCName) };
+	fro::Console::getInstance().log(message, fro::Console::BackgroundColor::darkGreen);
+#endif
+
+	return 0;
+}
+
+static int removeNPC(lua_State* const pState)
+{
+	auto const NPCName{ lua_tostring(pState, 1) };
+	NPCManager::getInstance().removeNPC(NPCName);
+
+#if not defined NDEBUG
+	auto const message{ std::format("NPC named {} removed!", NPCName) };
+	fro::Console::getInstance().log(message, fro::Console::BackgroundColor::darkRed);
+#endif
+
+	return 0;
+}
+
 int main()
 {
-    fro::CustomUniquePointer<lua_State> pLuaState{ luaL_newstate(), lua_close };
-    luaL_openlibs(pLuaState.get());
+	fro::CustomUniquePointer<lua_State> pState{ luaL_newstate(), lua_close };
+	luaL_openlibs(pState.get());
+
+	lua_register(pState.get(), "addNPC", addNPC);
+	lua_register(pState.get(), "removeNPC", removeNPC);
+
+	luaL_dofile(pState.get(), "Resources/startup.lub");
+
+#if not defined NDEBUG
+	std::cout << '\n';
+#endif
+
+	NPCManager::getInstance().dump();
 }
