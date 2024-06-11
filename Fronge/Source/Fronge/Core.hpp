88ce788ec5ead4acc79966505063b9ab190d87be
Froncu <jakubfratczak204@gmail.com>
2024-06-11 19:25:18 +0200

    [REFACTOR] remove the FRO_PLATFORM_WINDOWS preprocessor macro

--- Fronge/Source/Fronge/Core.hpp
+++ Fronge/Source/Fronge/Core.hpp
@@ -1,14 +1,10 @@
 #if not defined FRO_CORE_HPP
 #define FRO_CORE_HPP
 
-#if defined FRO_PLATFORM_WINDOWS
-	#if defined FRO_BUILD_DLL
-		#define FRO_API _declspec(dllexport)
-	#else
-		#define FRO_API _declspec(dllimport)
-	#endif
+#if defined FRO_BUILD_DLL
+	#define FRO_API _declspec(dllexport)
 #else
-	#error Fronge only supports Windows!
+	#define FRO_API _declspec(dllimport)
 #endif
 
 #endif
--- Fronge/Source/Fronge/EntryPoint.hpp
+++ Fronge/Source/Fronge/EntryPoint.hpp
@@ -1,15 +1,11 @@
 #if not defined FRO_ENTRY_POINT_HPP
 #define FRO_ENTRY_POINT_HPP
 
-#if defined FRO_PLATFORM_WINDOWS
-
 extern std::unique_ptr<fro::Application> fro::createApplication();
 
 int main(int, char**)
 {
 	fro::createApplication()->run();
 }
-
-#endif
 
 #endif
