e9f183a70cd097600b4dea39b27b9e204d59b16d
Froncu <jakubfratczak204@gmail.com>
2024-07-30 19:44:03 +0200

    [FEATURE] add a sprite component

---
+++ Fronge/Source/ECS/Components/Sprite.hpp
@@ -0,0 +1,19 @@
+#if not defined SPRITE_HPP
+#define SPRITE_HPP
+
+#include "ECS/Components/Component.hpp"
+#include "Maths/Structs/Rectangle.hpp"
+#include "Reference/Reference.hpp"
+#include "Resources/Texture.hpp"
+
+namespace fro
+{
+	struct Sprite final : public Component
+	{
+	public:
+		Reference<Texture const> texture{};
+		Rectangle<int> sourceRectangle{};
+	};
+}
+
+#endif
