da0e78e56773c46789d2098bd2e44a9dbb377566
Jakub Fratczak <jakubfratczak204@gmail.com>
2025-04-15 15:08:08 +0200

    [FEATURE] give the default logger functionality instead of treating it like a NULL provider

--- engine/source/services/logger/logger.cpp
+++ engine/source/services/logger/logger.cpp
@@ -2,8 +2,12 @@
 
 namespace fro
 {
-   void Logger::log(Type, bool, std::string_view)
+   void Logger::log(Type const type, bool const engine_level, std::string_view const message)
    {
-      std::cout << "no provider set for Logger!\n";
+      std::cout <<
+         std::format("[{}] [{}]: {}\n",
+            engine_level ? "ENGINE" : "APP",
+            type == Type::INFO ? "INFO" : type == Type::WARNING ? "WARNING" : "ERROR",
+            message);
    }
 }
