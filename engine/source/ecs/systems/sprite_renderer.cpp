7f02e612691f2fa1c12dc76c43e9a9f377c622d0
Froncu <jakubfratczak204@gmail.com>
2025-07-31 14:14:21 +0200

    [FEATURE] stop using the center of the window as the origin

--- engine/source/ecs/systems/sprite_renderer.cpp
+++ engine/source/ecs/systems/sprite_renderer.cpp
@@ -5,13 +5,8 @@ namespace fro
 {
    void SpriteRenderer::render(Scene const& scene) const
    {
-      Vector2 const half_resolution{ render_context_->resolution() * 0.5 };
       for (auto&& [_, sprite, transform] : scene.group<Pack<Sprite>, Pack<Transform>>())
          if (sprite.texture.valid())
-         {
-            TransformMatrix world_transform{ transform.world() };
-            world_transform.translate(half_resolution);
-            render_context_->render(*sprite.texture, world_transform, sprite.source_rectangle);
-         }
+            render_context_->render(*sprite.texture, transform.world(), sprite.source_rectangle);
    }
 }
