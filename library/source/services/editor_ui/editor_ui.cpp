3513bdc650baafcb1f11621029690be52d8247a6
Froncu <jakubfratczak204@gmail.com>
2025-09-10 09:23:38 +0200

    [BUILD] replace the FroGui submodule with the imgui package

--- library/source/services/editor_ui/editor_ui.cpp
+++ library/source/services/editor_ui/editor_ui.cpp
@@ -1,5 +1,5 @@
-#include <backends/imgui_impl_sdl3.h>
-#include <backends/imgui_impl_sdlrenderer3.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_sdlrenderer3.h>
 #include <imgui.h>
 
 #include "editor_ui.hpp"
